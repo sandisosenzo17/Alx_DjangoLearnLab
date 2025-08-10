4a4b164db11170a78c2d394264b87ceb07e40543
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-08-10 12:07:00 +0200

    Write python code to retrieve a library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,3 +1,9 @@
+from relationship_app.models import models
+
 #List all books in a library.
+library_name = models.Library.objects.create(name="Lib")
+models.Library.objects.get(name="Lib")
+
 #Query all books by a specific author.
+
 #Retrieve the librarian for a library.
