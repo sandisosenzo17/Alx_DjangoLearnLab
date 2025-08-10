8aa48b38d90a6b8562d6779065f401be193b0e9b
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-08-10 13:24:11 +0200

    Create a view with code and move the custom html file to templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
 from relationship_app.models import Author, Book, Librarian, Library
-from django.http import HttpResponse
-
 # Create your views here.
 def list_all_books(request):
   #Retrieve all books
-  books = Book.objects.all()
+  books = Book.objects.select_related.all()
 
-  lines = []
+  return render(request, "relationship/list_books.html", {'books': books})
 
-  #Prepare a view in a list form
-  for book in books:
-    author = book.author.name
-    line = book.title + " by " + author
-    lines.append(line)
-
-  if lines:
-    response_text = "\n".join(lines)
-  else:
-    response_text = "No books found"
   
-  return HttpResponse(response_text, content_type="text/plain")
