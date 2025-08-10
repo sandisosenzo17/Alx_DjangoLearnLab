c5a822b53077e4b83c598a0f542cad2db54cbc33
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-08-10 12:31:44 +0200

    Change variable names to match the checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,10 +5,10 @@ library_name = models.Library.objects.create(name="Lib")
 models.Library.objects.get(name=library_name)
 
 #Query all books by a specific author.
-author = models.Author.objects.create(name="Michio Kaku")
-book = models.Book.objects.create(title="The book of Mad Libs", author=author)
-book = models.Book.objects.create(title="The Physics of the Impossible", author=author)
+author_name = models.Author.objects.create(name="Michio Kaku")
+book1 = models.Book.objects.create(title="The book of Mad Libs", author=author_name)
+book2 = models.Book.objects.create(title="The Physics of the Impossible", author=author_name)
 books = models.Library.books.all()
-aut = models.Author.objects.get(name=author)
-bk = models.Book.objects.filter(author=aut)
+author = models.Author.objects.get(name=author_name)
+bk = models.Book.objects.filter(author=author)
 #Retrieve the librarian for a library.
