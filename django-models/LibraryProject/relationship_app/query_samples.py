3b51d62bcc446e32d1f891a4ce5c6cbf8588c594
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-08-10 12:21:57 +0200

    Query books by author name

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,5 +5,9 @@ library_name = models.Library.objects.create(name="Lib")
 models.Library.objects.get(name=library_name)
 
 #Query all books by a specific author.
+author = models.Author.objects.create(name="Michio Kaku")
+book = models.Book.objects.create(title="The book of Mad Libs", author=author)
+book = models.Book.objects.create(title="The Physics of the Impossible", author=author)
 books = models.Library.books.all()
+_books = models.Book.objects.filter(author__name = author)
 #Retrieve the librarian for a library.
