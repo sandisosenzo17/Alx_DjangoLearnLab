f2b2a29cd0c748ce68b3295915982df056a8d39f
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-08-10 12:10:51 +0200

    Set a related name for Book and retrieve all books in a library

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -9,7 +9,7 @@ class Author(models.Model):
 
 class Book(models.Model):
   title = models.CharField(max_length=200) 
-  author = models.ForeignKey(Author, on_delete=models.CASCADE)
+  author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
   def __str__(self):
     return self.title
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,9 +1,9 @@
 from relationship_app.models import models
 
 #List all books in a library.
-library_name = models.Library.objects.create(name="Lib")
+library = models.Library.objects.create(name="Lib")
 models.Library.objects.get(name="Lib")
 
 #Query all books by a specific author.
-
+books = library.books.all()
 #Retrieve the librarian for a library.
