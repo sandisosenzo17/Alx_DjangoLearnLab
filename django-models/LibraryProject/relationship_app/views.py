e64345dd658b53ec6766f68b0c389973739e7458
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-08-17 21:24:36 +0200

    Start importing Library before Book model inside views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library, Book
 
 # Function-based view
 def list_all_books(request):
