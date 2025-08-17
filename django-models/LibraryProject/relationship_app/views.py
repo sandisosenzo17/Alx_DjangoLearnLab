a5d7d0dd73410cca38800518b985e987ce787408
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-08-17 21:52:06 +0200

    Setup login, registrationa dn logout authentiacation

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Library, Book
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 
 # Function-based view
 def list_books(request):
@@ -13,3 +16,13 @@ class LibraryDetailView(DetailView):
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log user in immediately after registration
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
