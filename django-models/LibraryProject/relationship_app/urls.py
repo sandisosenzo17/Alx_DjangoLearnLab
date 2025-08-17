4ccad650ca678808349d1cf914da7f18d092d1ad
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-08-17 22:08:54 +0200

    Redirect logout functionality to the logout template

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
     path("login/", auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
 ]
 
