8e900f1ea5ddf80ae92f3c74938555ef827efacc
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-08-17 20:30:39 +0200

    Fix imports in the query_samples.py file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,16 @@
-from relationship_app.models import models
+from relationship_app.models import Author, Book, Library, Librarian
 
 #List all books in a library.
-library_name = models.Library.objects.create(name="Lib")
-models.Library.objects.get(name=library_name)
+library_name = Library.objects.create(name="Lib")
+Library.objects.get(name=library_name)
 
 #Query all books by a specific author.
-author_name = models.Author.objects.create(name="Michio Kaku")
-book1 = models.Book.objects.create(title="The book of Mad Libs", author=author_name)
-book2 = models.Book.objects.create(title="The Physics of the Impossible", author=author_name)
-books = models.Library.books.all()
-author = models.Author.objects.get(name=author_name)
-bk = models.Book.objects.filter(author=author)
+author_name = Author.objects.create(name="Michio Kaku")
+book1 = Book.objects.create(title="The book of Mad Libs", author=author_name)
+book2 = Book.objects.create(title="The Physics of the Impossible", author=author_name)
+books = Library.books.all()
+author = Author.objects.get(name=author_name)
+bk = Book.objects.filter(author=author)
 
 #Retrieve the librarian for a library.
-models.Librarian.objects.get(library=library_name)
+Librarian.objects.get(library=library_name)
