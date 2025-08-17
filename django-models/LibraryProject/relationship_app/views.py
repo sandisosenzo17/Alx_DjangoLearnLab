645f0e79c0c6fdf844195bf1a9940d824ba366c2
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-08-17 21:20:57 +0200

    Change the method of importing models inside the views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from relationship_app.models import Book, Library
+from .models import Book, Library
 
 # Function-based view
 def list_all_books(request):
