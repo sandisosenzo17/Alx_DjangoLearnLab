9074b6d31c3344c41023a48917e7f897c0f331f2
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-08-17 20:33:08 +0200

    Give parameters to the selected_related() function in views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from relationship_app.models import Author, Book, Librarian, Library
 # Create your views here.
 def list_all_books(request):
   #Retrieve all books
-  books = Book.objects.select_related.all()
+  books = Book.objects.select_related('author').all()
 
   return render(request, "relationship/list_books.html", {'books': books})
 
