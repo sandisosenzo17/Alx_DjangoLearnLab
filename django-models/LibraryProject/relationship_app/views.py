8c56c52f712b5ab447b8187ff804e90f0637a37f
sandisosenzo17 <sandisosenzo17@gmail.com>
2025-08-17 21:27:22 +0200

    Modify how DetailView is imported for the class-based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library, Book
 
 # Function-based view
@@ -12,3 +12,4 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
